72b980a514970135f82b3f4211020d80e9077395
BrunoSilva <BrunoSiFe@gmail.com>
2018-09-21 11:30:19 -0300

    Projeto Criado

---
+++ src/test/java/com/aula/udemy/cursoudemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aula.udemy.cursoudemy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoUdemyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
