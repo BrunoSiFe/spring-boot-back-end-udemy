3e3fd3384e5a10de1c3123d5b8da72f36cf3cc2d
BrunoSilva <BrunoSiFe@gmail.com>
2018-09-21 12:03:58 -0300

    Adiciona cateoria e metodo de listar categorias

--- src/main/java/com/aula/udemy/cursoudemy/controller/CategoriaController.java
+++ src/main/java/com/aula/udemy/cursoudemy/controller/CategoriaController.java
@@ -1,8 +1,13 @@
 package com.aula.udemy.cursoudemy.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aula.udemy.cursoudemy.entities.CategoriaEntity;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,7 +15,16 @@ public class CategoriaController {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<CategoriaEntity> listar() {
+		
+		CategoriaEntity cat1 = new CategoriaEntity(1,"Informática");
+		CategoriaEntity cat2 = new CategoriaEntity(2,"Calçados");
+		
+		List<CategoriaEntity> listaCategorias = new ArrayList<>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
+		
 	}
 }
